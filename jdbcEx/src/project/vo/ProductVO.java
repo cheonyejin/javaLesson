fc05f13020eba402c33a765b898f69c16492d151
wmj <tmanfektjt25@gmail.com>
2024-01-30 17:35:25 +0900

    project.vo소스

---
+++ jdbcEx/src/project/vo/ProductVO.java
@@ -0,0 +1,28 @@
+package project.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+//loombook이용하기 
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+
+public class ProductVO { 
+
+    public String pcode;
+    public String category;
+    public String pname;
+    public int price;
+
+    @Override
+    public String toString() {
+        return String.format("%6s %15s %40s\t %,8d", category,pcode,pname,price);
+    }
+
+    
+
+
+    
+}
